2967fc41e07f49aad02138ca65504b7fc20e6a46
Jayden Doll <jayjdoll002@gmail.com>
2022-10-30 17:21:43 -0500

    temporarily remove tileClassName functionality

--- src/components/yoga-calendar/yoga-calendar.component.jsx
+++ src/components/yoga-calendar/yoga-calendar.component.jsx
@@ -49,18 +49,7 @@ const YogaCalendar = () => {
         maxDate={currentDate}
         showFixedNumberOfWeeks={true}
         value={selectedDate}
-        tileClassName={({ date }) => {
-          const result = Object.entries({ parsedLocalStorageData });
-
-          const makeNewDate = () => {
-            return date.getDate().toLocaleString().replaceAll("/", "");
-          };
-
-          const newDate = makeNewDate();
-          if (result.find((x) => x[1].VIDEO_STATUS)) {
-            console.log(result);
-          }
-        }}
+        tileClassName={({ date }) => {}}
       />
     </Wrapper>
   );
